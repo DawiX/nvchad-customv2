120865865adfbb8237d65edf235cbc04f9876db5
David Pech <dpech@chemaxon.com>
2026-07-20 01:45:29 +0200

    Fix: nvimtree git icon rendering

--- lua/plugins/nvimtree.lua
+++ lua/plugins/nvimtree.lua
@@ -4,19 +4,6 @@ return {
     opts = {
       git = {
         enable = true,
-        ignore = false,
-        show_on_dirs = true,
-        show_on_open_dirs = true,
-      },
-      filesystem_watchers = {
-        enable = true,
-        debounce_delay = 50,
-        ignore_dirs = { "node_modules", ".git" },
-      },
-      actions = {
-        open_file = {
-          resize_window = true,
-        },
       },
       renderer = {
         highlight_git = "name",
@@ -24,29 +11,8 @@ return {
           show = {
             git = true,
           },
-          glyphs = {
-            git = {
-              unstaged = "✗",
-              staged = "✓",
-              unmerged = "",
-              renamed = "➜",
-              untracked = "★",
-              deleted = "",
-              ignored = "◌",
-            },
-          },
         },
       },
     },
-    init = function()
-      -- Auto-refresh nvim-tree on focus and after git commands
-      vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter" }, {
-        callback = function()
-          if vim.fn.exists(":NvimTreeRefresh") == 2 then
-            vim.cmd("NvimTreeRefresh")
-          end
-        end,
-      })
-    end,
   },
 }
